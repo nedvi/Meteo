952a92ce441d2d43a10c8ed27ebd05838bdb24c2
Dominik Nedved <nedvi2001@gmail.com>
2023-12-08 09:04:32 +0100

    v0.2 - Current Weather Panel

--- meteo/javascript.js
+++ meteo/javascript.js
@@ -1,19 +1,57 @@
+const API_KEY = "6713db4b439848a197b83550231705";
+let city = "Plzeň";
 
-async function getActualWeather() {
-    const requestURL = "https://api.weatherapi.com/v1/current.json?key=6713db4b439848a197b83550231705&q=Plzeň&lang=cs";
+async function getCurrentWeather() {
+    const requestURL = "https://api.weatherapi.com/v1/current.json?key=" + API_KEY + "&q="+ city + "&lang=cs";
     const request = new Request(requestURL);
 
     const response = await fetch(request);
-    const actualWeather = await response.json();
+    const currentWeather = await response.json();
 
-    document.getElementById("favicon").href = "https:" + actualWeather.current.condition.icon;
+    document.getElementById("favicon").href = "https:" + currentWeather.current.condition.icon;
 
-    document.getElementById("city").innerHTML = actualWeather.location.name;
-    document.getElementById("temp").innerHTML = actualWeather.current.temp_c + "°C";
-    document.getElementById("feelslikeTemp").innerHTML = "Pocitově: " + actualWeather.current.feelslike_c + "°C";
-    document.getElementById("weatherText").innerHTML = actualWeather.current.condition.text;
-    document.getElementById("icon").src = "https:" + actualWeather.current.condition.icon;
-    document.getElementById("lastUpdated").innerHTML = "Poslední aktualizace: " + actualWeather.current.last_updated;
+    document.getElementById("city").innerHTML = currentWeather.location.name;
+    document.getElementById("temp").innerHTML = currentWeather.current.temp_c + "°C";
+    document.getElementById("feelslikeTemp").innerHTML = "Pocitově: " + currentWeather.current.feelslike_c + "°C";
+    document.getElementById("weatherText").innerHTML = currentWeather.current.condition.text;
+    document.getElementById("icon").src = "https:" + currentWeather.current.condition.icon;
+    document.getElementById("lastUpdated").innerHTML = "Poslední aktualizace: " + currentWeather.current.last_updated;
+
 }
 
-getActualWeather();
+async function getForecastWeather() {
+    const requestURL = "https://api.weatherapi.com/v1/forecast.json?key=" + API_KEY + "&q=" + city + "&days=" + 7;
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const forecastWeather = await response.json();
+
+    document.getElementById("currentRain").innerHTML = forecastWeather.current.precip_mm + " mm";
+    document.getElementById("currentSunrise").innerHTML = (forecastWeather.forecast.forecastday[0].astro.sunrise);
+    document.getElementById("currentSunset").innerHTML = (forecastWeather.forecast.forecastday[0].astro.sunset);
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("clock").innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }  // add zero in front of numbers < 10
+    return i;
+}
+
+getCurrentWeather();
+getForecastWeather();
+function autoUpdateCurrentWeather() {
+    setInterval(getCurrentWeather, 5 * 60 * 1000)
+    setInterval(getForecastWeather, 5 * 60 * 1000)
+}
