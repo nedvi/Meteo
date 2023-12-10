8d3a2d1b8f270fe5d8cb3c9ddccbee4959822998
Dominik Nedved <nedvi2001@gmail.com>
2023-12-10 09:33:55 +0100

    v0.3 - Working search bar; New Current weather Sections

--- meteo/javascript.js
+++ meteo/javascript.js
@@ -10,7 +10,7 @@ async function getCurrentWeather() {
 
     document.getElementById("favicon").href = "https:" + currentWeather.current.condition.icon;
 
-    document.getElementById("city").innerHTML = currentWeather.location.name;
+    document.getElementById("city").innerHTML = currentWeather.location.name + ", " + currentWeather.location.region;
     document.getElementById("temp").innerHTML = currentWeather.current.temp_c + "°C";
     document.getElementById("feelslikeTemp").innerHTML = "Pocitově: " + currentWeather.current.feelslike_c + "°C";
     document.getElementById("weatherText").innerHTML = currentWeather.current.condition.text;
@@ -20,15 +20,29 @@ async function getCurrentWeather() {
 }
 
 async function getForecastWeather() {
-    const requestURL = "https://api.weatherapi.com/v1/forecast.json?key=" + API_KEY + "&q=" + city + "&days=" + 7;
+    const requestURL = "https://api.weatherapi.com/v1/forecast.json?key=" + API_KEY + "&q=" + city + "&days=" + 7 + "&lang=cs";
     const request = new Request(requestURL);
 
     const response = await fetch(request);
     const forecastWeather = await response.json();
 
     document.getElementById("currentRain").innerHTML = forecastWeather.current.precip_mm + " mm";
-    document.getElementById("currentSunrise").innerHTML = (forecastWeather.forecast.forecastday[0].astro.sunrise);
-    document.getElementById("currentSunset").innerHTML = (forecastWeather.forecast.forecastday[0].astro.sunset);
+    document.getElementById("currentSunrise").innerHTML = forecastWeather.forecast.forecastday[0].astro.sunrise;
+    document.getElementById("currentSunset").innerHTML = forecastWeather.forecast.forecastday[0].astro.sunset;
+
+    for (let i = 1; i <= 3; i++) {
+        let dayOne = getDayOfWeekString(forecastWeather.forecast.forecastday[i-1].date);
+        document.getElementById("day" + i).innerHTML = dayOne.charAt(0).toUpperCase() + dayOne.slice(1);
+
+        document.getElementById("forecastDay" + i + "Icon").src = "https:" + (forecastWeather.forecast.forecastday[i-1].day.condition.icon);
+        document.getElementById("forecastDay" + i + "Text").innerHTML = forecastWeather.forecast.forecastday[i-1].day.condition.text;
+        document.getElementById("forecastDay" + i + "Temp").innerHTML = forecastWeather.forecast.forecastday[i-1].day.avgtemp_c + "°C";
+        document.getElementById("forecastDay" + i + "MaxTemp").innerHTML = forecastWeather.forecast.forecastday[i-1].day.maxtemp_c + "°C";
+        document.getElementById("forecastDay" + i + "MinTemp").innerHTML = forecastWeather.forecast.forecastday[i-1].day.mintemp_c + "°C";
+        document.getElementById("forecastDay" + i + "Rain").innerHTML = forecastWeather.forecast.forecastday[i-1].day.totalprecip_mm + " mm";
+        document.getElementById("forecastDay" + i + "Sunrise").innerHTML = forecastWeather.forecast.forecastday[i-1].astro.sunrise;
+        document.getElementById("forecastDay" + i + "Sunset").innerHTML = forecastWeather.forecast.forecastday[i-1].astro.sunset;
+    }
 }
 
 function startTime() {
@@ -49,9 +63,24 @@ function checkTime(i) {
     return i;
 }
 
+function getDayOfWeekString(dateString) {
+    return new Date(dateString).toLocaleString('CS-CZ', {weekday: 'long'});
+}
+
 getCurrentWeather();
 getForecastWeather();
 function autoUpdateCurrentWeather() {
     setInterval(getCurrentWeather, 5 * 60 * 1000)
     setInterval(getForecastWeather, 5 * 60 * 1000)
 }
+
+function updateCity(form) {
+    city = form.newCityInput.value;
+    form.newCityInput.value = "";
+
+    getCurrentWeather();
+    getForecastWeather()
+}
+
+
+
